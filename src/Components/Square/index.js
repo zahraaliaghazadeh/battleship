8f78c0191ae4c10acfa6aeb6836b500e965d98e8
zahraaghazadeh <zahraaliaghazadeh@gmail.com>
2021-11-07 23:10:56 -0800

    fixed some things on navbar and created square

---
+++ src/Components/Square/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style.css";
+
+
+export default function Square() {
+
+    return (
+
+        <div>
+            <div className="square">
+            </div>
+           
+        </div>
+
+    );
+}
