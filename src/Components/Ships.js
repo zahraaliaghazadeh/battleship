45480e7c479a62876b23ca5a6449ff26d77f3d74
Dhunt5 <deborahjhunt@att.net>
2021-11-14 17:15:03 -0800

    Grid and hamburger code

---
+++ src/Components/Ships.js
@@ -0,0 +1,47 @@
+export const shipTypes = [
+    {
+      id: 1,
+      name: 'Carrier',
+      size: 5,
+      count: 1,
+      position: {},
+      hitPoints: 5,
+      hits: 0
+    },
+    {
+      id: 2,
+      name: 'Battleship',
+      size: 4,
+      count: 1,
+      position: {},
+      hitPoints: 4,
+      hits: 0
+    },
+    {
+      id: 3,
+      name: 'Cruiser',
+      size: 3,
+      count: 1,
+      position: {},
+      hitPoints: 3,
+      hits: 0
+    },
+    {
+      id: 4,
+      name: 'Submarine',
+      size: 3,
+      count: 1,
+      position: {},
+      hitPoints: 3,
+      hits: 0
+    },
+    {
+      id: 5,
+      name: 'Patrol',
+      size: 2,
+      count: 1,
+      position: {},
+      hitPoints: 2,
+      hits: 0
+    }
+  ];
