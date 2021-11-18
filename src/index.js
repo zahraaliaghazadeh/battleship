01148d0c049e9e41b803b7db7461757cd16e4750
zahraaghazadeh <zahraaliaghazadeh@gmail.com>
2021-11-17 18:46:35 -0800

    wip deploy2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducers from './Reducers/reducers';
+import reducers from './reducers/reducers';
 
 const store = createStore(reducers); 
 
