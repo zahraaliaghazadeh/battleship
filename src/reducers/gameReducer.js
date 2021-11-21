4507e8f797a4fcacb3e8975d4373aab1830b516c
zahraaghazadeh <zahraaliaghazadeh@gmail.com>
2021-11-20 16:51:31 -0800

    wip

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,4 +1,5 @@
-import generateFleet from '../Components/generateShips'
+import generateFleet from '../Components/generateShips';
+import Board from "../Components/Board/Board";
 
 const defaultState = [
     ['lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare'],
@@ -11,23 +12,16 @@ const defaultState = [
     ['lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare'],
     ['lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare'],
     ['lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare', 'lightBlueSquare'],
-
 ];
 let fleetArr = generateFleet()
 
 export default function gameReducer(state = defaultState, action) {
     if (action.type === 'boardClick') {
         const value = state[action.x][action.y];
-        // if (value === 'lightBlueSquare') {
-        //     state[action.x][action.y] = 'lightGreenSquare';
-        // } else{
-        //     state[action.x][action.y] = 'lightBlueSquare';
-        // } 
 
         // let shipSquareCounter = 0;
-        // let gameOver = checkAllSunk()
-        // while(gameOver != true){
-
+        let gameOver = checkAllSunk()
+        while(gameOver != true){
 
             for (let k = 0; k < 10; k++) {
                 for (let l = 0; l < 10; l++) {
@@ -47,16 +41,22 @@ export default function gameReducer(state = defaultState, action) {
                         state[action.x][action.y] = 'darkBlueSquare';
                         // console.log("this was a miss")
                     }
+                    // else if ((value != 'lightBlueSquare')){
+                    //     state[action.x][action.y] = 'redSquare';
+                    // }
 
                     // console.log("the number of hit ship squares is: ", shipSquareCounter)
 
                 }
             }
 
-        // }
+        }
             return [...state];
         
     }
+
+
+ 
 
 
     // if (action.type === 'RESET' || action.type === 'RESET_GAMEBOARD_ONLY') {
@@ -84,17 +84,29 @@ export default function gameReducer(state = defaultState, action) {
 }
 
 
-// function checkAllSunk(board) {
-//     let sunkSqCounter = 0;
-//     for (let m = 0; m < 10; m++) {
-//         for (let n = 0; n < 10; n++) {
-//             if (board[m][n] === 'lightGreenSquare' || board[m][n] === 'darkGreenSquare') {
-//                 sunkSqCounter++;
-//             }
-//         }
-//     }
-//     if (sunkSqCounter >= 17) {
-//         console.log("game over")
-//         return true
-//     }
+function checkAllSunk(Board) {
+    let sunkSqCounter = 0;
+    for (let m = 0; m < 10; m++) {
+        for (let n = 0; n < 10; n++) {
+            if (Board[m][n] === 'lightGreenSquare' || Board[m][n] === 'darkGreenSquare') {
+                sunkSqCounter++;
+            }
+        }
+    }
+    if (sunkSqCounter >= 17) {
+        console.log("game over")
+        return true
+    }
+}
+
+// function changeBackground(e) {
+//     e.target.style.background = 'red';
+  
+
+//   return (
+//     <div className="App">
+//       <button onMouseOver={changeBackground}>Hover over me!</button>
+//     </div>
+//   );
+
 // }
